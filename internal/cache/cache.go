a1accbebbeb0f96182f23842f62f0a59168fcd7f
Thomas Poignant <thomas.poignant@adevinta.com>
2021-01-06 11:31:22 +0100

    WIP

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"fmt"
+	"github.com/google/go-cmp/cmp"
 	"gopkg.in/yaml.v2"
 	"sync"
 
@@ -32,6 +34,8 @@ func UpdateCache(loadedFlags []byte) error {
 		return err
 	}
 
+	go FlagChanges(FlagsCache, flags)
+
 	mutex.Lock()
 	FlagsCache = flags
 	mutex.Unlock()
@@ -41,4 +45,41 @@ func UpdateCache(loadedFlags []byte) error {
 // Close is removing everything from the cache.
 func Close() {
 	FlagsCache = nil
+}
+
+func FlagChanges(oldCache map[string]flags.Flag, newCache map[string]flags.Flag) map[string]flags.Flag {
+	for key := range oldCache {
+		_, inNewCache := newCache[key]
+
+		if !inNewCache {
+			fmt.Printf("flag %v removed\n", key)
+			continue
+		}
+
+		if oldCache[key].Disable != newCache[key].Disable {
+			if newCache[key].Disable {
+				// Flag is disabled
+				fmt.Printf("The flag %v is turned off\n", key)
+			} else {
+				flag := newCache[key]
+				fmt.Printf(
+					"The flag %s is turned ON \n\t"+
+						"- percentage is %d%% \n\t"+
+						"- rule is [%s] \n", key, flag.Percentage, flag.Rule)
+			}
+		} else if !cmp.Equal(oldCache[key], newCache[key]) {
+			// key has changed in cache
+			fmt.Printf("Flag %v hasChanged: %v\n", key, cmp.Diff(oldCache[key], newCache[key]))
+		}
+	}
+
+	for key := range newCache {
+		_, inOldCache := oldCache[key]
+		if !inOldCache && !newCache[key].Disable {
+			fmt.Printf("flag %v addedd\n", key)
+		}
+	}
+
+	// TODO: valid return
+	return make(map[string]flags.Flag)
 }
