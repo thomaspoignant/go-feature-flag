a776773e305012a5f2825b0f2d4a8fec71b325e7
Thomas Poignant <thomas.poignant@adevinta.com>
2021-04-09 19:30:32 +0200

    add godoc for the config object

--- config_collector.go
+++ config_collector.go
@@ -6,9 +6,20 @@ import (
 	"github.com/thomaspoignant/go-feature-flag/internal/exporter"
 )
 
-// TODO : Add documentation
+// DataExporter is the configuration of your export target.
 type DataExporter struct {
-	FlushInterval   time.Duration
+	// FlushInterval is the interval we are waiting to export the data.
+	// example: if you set your FlushInterval to 1 minutes, we will send
+	// the data every minute unless we reach the max event in cache before.
+	FlushInterval time.Duration
+
+	// MaxEventInCache is the maximum number of event you keep in the cache
+	// before sending the data to the Exporter.
+	// We will send the data when the MaxEventInCache is reach or if we have
+	// waited the FlushInterval.
 	MaxEventInCache int64
-	Exporter        exporter.Exporter
+
+	// Exporter is the configuration of your exporter.
+	// You can see all available exporter in the ffexporter package.
+	Exporter exporter.Exporter
 }
