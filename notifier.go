5e4194b05f3cc2c278f6ead6090e00c7cc34d788
Thomas Poignant <thomas.poignant@gmail.com>
2021-04-26 09:51:23 +0200

    Remove deprecated Webhooks field (#99)

--- notifier.go
+++ notifier.go
@@ -1,11 +1,6 @@
 package ffclient
 
 import (
-	"net/http"
-	"net/url"
-	"os"
-	"time"
-
 	"github.com/thomaspoignant/go-feature-flag/internal/notifier"
 )
 
@@ -25,46 +20,5 @@ func getNotifiers(config Config) ([]notifier.Notifier, error) {
 		notifiers = append(notifiers, notifier)
 	}
 
-	// Deprecated: we will have to remove that block when webhook will be un-supported
-	wh, err := getWebhooks(config)
-	if err != nil {
-		return nil, err
-	}
-	notifiers = append(notifiers, wh...)
-	// end deprecated
-
 	return notifiers, nil
-}
-
-// Deprecated: use getNotifiers instead
-func getWebhooks(config Config) ([]notifier.Notifier, error) {
-	res := make([]notifier.Notifier, len(config.Webhooks))
-	for index, whConf := range config.Webhooks {
-		// httpClient used to call the webhook
-		httpClient := http.Client{
-			Timeout: 10 * time.Second,
-		}
-
-		// Deal with meta informations
-		if whConf.Meta == nil {
-			whConf.Meta = make(map[string]string)
-		}
-		hostname, _ := os.Hostname()
-		whConf.Meta["hostname"] = hostname
-
-		endpointURL, err := url.Parse(whConf.EndpointURL)
-		if err != nil {
-			return nil, err
-		}
-
-		w := notifier.WebhookNotifier{
-			Logger:      config.Logger,
-			EndpointURL: *endpointURL,
-			Secret:      whConf.Secret,
-			Meta:        whConf.Meta,
-			HTTPClient:  &httpClient,
-		}
-		res[index] = &w
-	}
-	return res, nil
 }
