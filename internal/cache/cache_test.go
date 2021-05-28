6a3e82bdd262a397f703509730f78ab59475401c
Thomas Poignant <thomas.poignant@adevinta.com>
2021-05-28 16:07:45 +0200

    Add test for cache AllFlags

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -186,3 +186,123 @@ disable = false`),
 		})
 	}
 }
+
+func Test_AllFlags(t *testing.T) {
+	yamlFile := []byte(`test-flag:
+  rule: key eq "random-key"
+  percentage: 100
+  true: true
+  false: false
+  default: false
+  trackEvents: false
+`)
+
+	type args struct {
+		loadedFlags []byte
+	}
+	tests := []struct {
+		name       string
+		args       args
+		expected   map[string]model.FlagData
+		wantErr    bool
+		flagFormat string
+	}{
+		{
+			name:       "Yaml valid",
+			flagFormat: "yaml",
+			args: args{
+				loadedFlags: yamlFile,
+			},
+			expected: map[string]model.FlagData{
+				"test-flag": {
+					Disable:     nil,
+					Rule:        testconvert.String("key eq \"random-key\""),
+					Percentage:  testconvert.Float64(100),
+					True:        testconvert.Interface(true),
+					False:       testconvert.Interface(false),
+					Default:     testconvert.Interface(false),
+					TrackEvents: testconvert.Bool(false),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:       "Yaml multiple flags",
+			flagFormat: "yaml",
+			args: args{
+				loadedFlags: []byte(`test-flag:
+  rule: key eq "random-key"
+  percentage: 100
+  true: true
+  false: false
+  default: false
+  trackEvents: false
+test-flag2:
+  rule: key eq "random-key"
+  percentage: 0
+  true: "true"
+  false: "false"
+  default: "false"
+  trackEvents: false
+`),
+			},
+			expected: map[string]model.FlagData{
+				"test-flag": {
+					Disable:     nil,
+					Rule:        testconvert.String("key eq \"random-key\""),
+					Percentage:  testconvert.Float64(100),
+					True:        testconvert.Interface(true),
+					False:       testconvert.Interface(false),
+					Default:     testconvert.Interface(false),
+					TrackEvents: testconvert.Bool(false),
+				},
+				"test-flag2": {
+					Disable:     nil,
+					Rule:        testconvert.String("key eq \"random-key\""),
+					Percentage:  testconvert.Float64(0),
+					True:        testconvert.Interface("true"),
+					False:       testconvert.Interface("false"),
+					Default:     testconvert.Interface("false"),
+					TrackEvents: testconvert.Bool(false),
+				},
+			},
+			wantErr: false,
+		}, {
+			name:       "empty",
+			flagFormat: "yaml",
+			args: args{
+				loadedFlags: []byte(``),
+			},
+			expected: map[string]model.FlagData{},
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fCache := cache.New(cache.NewNotificationService([]notifier.Notifier{}))
+			_ = fCache.UpdateCache(tt.args.loadedFlags, tt.flagFormat)
+
+			allFlags, err := fCache.AllFlags()
+			if tt.wantErr {
+				assert.Error(t, err, "UpdateCache() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.NoError(t, err)
+
+			// If no error we compare with expected
+			for key, expected := range tt.expected {
+				got := allFlags[key]
+				assert.Equal(t, expected.GetRule(), got.GetRule())
+				assert.Equal(t, expected.GetPercentage(), got.GetPercentage())
+				assert.Equal(t, expected.GetTrue(), got.GetTrue())
+				assert.Equal(t, expected.GetFalse(), got.GetFalse())
+				assert.Equal(t, expected.GetDefault(), got.GetDefault())
+				assert.Equal(t, expected.GetTrackEvents(), got.GetTrackEvents())
+				assert.Equal(t, expected.GetDisable(), got.GetDisable())
+				assert.Equal(t, expected.GetRollout(), got.GetRollout())
+			}
+			fCache.Close()
+		})
+	}
+}
