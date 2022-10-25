809fcaecbb65342b23d859bfce650444a6383653
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2022-10-25 14:35:18 +0200

    Add newsletter on footer
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

---
+++ docs-v2/src/theme/Footer/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+import styles from './styles.module.css'
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+export default function FooterWrapper(props) {
+  const page = window.location;
+  const {siteConfig} = useDocusaurusContext();
+  const url = `${siteConfig.customFields.mailchimpURL};SIGNUP=${page}`;
+
+  const CustomForm = ({ status, message, onValidated }) => {
+    let email;
+    const submit = () =>
+      email &&
+      email.value.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email.value,
+      });
+
+    return (
+      <div>
+        <input className={styles.emailInput} ref={node => (email = node)} type="email" placeholder="Email" />
+        <button className="pushy__btn pushy__btn--df pushy__btn--black" onClick={submit}>
+          Subscribe <i className="fa-regular fa-paper-plane"></i>
+        </button>
+        {status === "error" && (<div className={clsx(styles.newsletterMessage, styles.error)} dangerouslySetInnerHTML={{ __html: message }} />)}
+        {status === "success" && (<div className={clsx(styles.newsletterMessage, styles.success)} dangerouslySetInnerHTML={{ __html: message }} />)}
+      </div>
+    );
+  };
+
+  const NewsletterForm = () => (
+    <div className={styles.newsletter}>
+      <h1>Get the latest GO Feature Flag updates</h1>
+      <MailchimpSubscribe
+        url={url}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
+          />
+        )}
+      />
+    </div>
+  )
+
+  return (
+    <>
+      <NewsletterForm />
+      <Footer {...props} />
+    </>
+  );
+}
