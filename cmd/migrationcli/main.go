5e62d288f2520d54e4b1987e660af56bd92bcb6b
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-01-13 15:25:55 +0100

    Code style
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- cmd/migrationcli/main.go
+++ cmd/migrationcli/main.go
@@ -12,11 +12,10 @@ import (
 
 func main() {
 	var opts struct {
-		InputFile   string `short:"f" long:"input-file" description:"Location of the flag file you want to convert." required:"true"` //nolint: lll
-		InputFormat string `long:"input-format" description:"Format of your input file (YAML, JSON or TOML)" required:"true"`         //nolint: lll
-
-		OutputFile   string `short:"o" long:"output-file" description:"Location of the converted flag file." required:"false"`    //nolint: lll
-		OutputFormat string `long:"output-format" description:"Format of your output file (YAML, JSON or TOML)" required:"false"` //nolint: lll
+		InputFile    string `short:"f" long:"input-file" description:"Location of the flag file you want to convert." required:"true"` //nolint: lll
+		InputFormat  string `long:"input-format" description:"Format of your input file (YAML, JSON or TOML)" required:"true"`         //nolint: lll
+		OutputFile   string `short:"o" long:"output-file" description:"Location of the converted flag file." required:"false"`         //nolint: lll
+		OutputFormat string `long:"output-format" description:"Format of your output file (YAML, JSON or TOML)" required:"false"`      //nolint: lll
 	}
 	_, err := flags.Parse(&opts)
 	if err != nil {
