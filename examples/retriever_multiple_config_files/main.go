9115c62e79106a0b027be8140e49402f6e89a2e1
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-03-17 17:12:08 +0100

    Add example for multiple retrievers
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

---
+++ examples/retriever_multiple_config_files/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/thomaspoignant/go-feature-flag/retriever"
+	"log"
+	"os"
+	"time"
+
+	"github.com/thomaspoignant/go-feature-flag/exporter/fileexporter"
+	"github.com/thomaspoignant/go-feature-flag/retriever/fileretriever"
+
+	ffclient "github.com/thomaspoignant/go-feature-flag"
+	"github.com/thomaspoignant/go-feature-flag/ffuser"
+)
+
+func main() {
+	// Init ffclient with multiple file retrievers.
+	err := ffclient.Init(ffclient.Config{
+		PollingInterval: 10 * time.Second,
+		Logger:          log.New(os.Stdout, "", 0),
+		Context:         context.Background(),
+		Retrievers: []retriever.Retriever{
+			&fileretriever.Retriever{
+				Path: "examples/retriever_multiple_config_files/flags.yaml",
+			},
+			&fileretriever.Retriever{
+				Path: "examples/retriever_multiple_config_files/flags2.yaml",
+			},
+		},
+		DataExporter: ffclient.DataExporter{
+			FlushInterval:    1 * time.Second,
+			MaxEventInMemory: 100,
+			Exporter: &fileexporter.Exporter{
+				OutputDir: "./",
+			},
+		},
+	})
+	// Check init errors.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer closing ffclient
+	defer ffclient.Close()
+
+	// create users
+	user1 := ffuser.NewAnonymousUser("aea2fdc1-b9a0-417a-b707-0c9083de68e3")
+
+	// user1
+	user1HasAccessToNewAdmin, err := ffclient.BoolVariation("new-admin-access", user1, false)
+	if err != nil {
+		// we log the error, but we still have a meaningful value in user1HasAccessToNewAdmin (the default value).
+		log.Printf("something went wrong when getting the flag: %v", err)
+	}
+	if !user1HasAccessToNewAdmin {
+		fmt.Println("user1 should not have access to the new admin because we are using the configuration from flag2.yaml")
+	}
+}
