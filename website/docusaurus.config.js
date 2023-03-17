f89ef6458fd82ce37831bbbc8780a82b26b12fc5
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-03-17 20:01:58 +0100

    edit blog edit url
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thomaspoignant/go-feature-flag/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/thomaspoignant/go-feature-flag/tree/main/website/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
