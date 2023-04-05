7ac81e82b4d6eda0caf881c85aaddab1b8e3ef39
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-04-05 21:55:56 +0200

    Update retriever doc
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

---
+++ website/src/components/doc/retrievers/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css'
+
+export function Retrievers() {
+  return(
+    <div className={styles.retriever}>
+      YO
+    </div>
+  );
+}
