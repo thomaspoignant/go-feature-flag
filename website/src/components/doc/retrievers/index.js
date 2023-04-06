522acaa555465b4d679a22d72ad45882078e16a1
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-04-06 15:24:57 +0200

    Add new version of the Where to store your flag page
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- website/src/components/doc/retrievers/index.js
+++ website/src/components/doc/retrievers/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import styles from './styles.module.css'
+import clsx from "clsx";
+import styles from "@site/src/components/doc/retrievers/styles.module.css";
+import Link from "@docusaurus/Link";
 
-export function Retrievers() {
+export function Retrievers(props) {
+
+  const listItems = props.test.map((item, index) =>  <Retriever {...item} key={index} />);
+
   return(
-    <div className={styles.retriever}>
-      YO
+    <div className="grid grid-pad">
+      {listItems}
+    </div>
+  );
+}
+
+function Retriever(item){
+  return(
+    <div className={clsx("col-1-3 mobile-col-1-2", styles.container)}>
+      <div className={styles.card}>
+        <img src={item.logo} className={styles.cardLogo} />
+          <div className={styles.cardDetails}>
+            <div className={styles.title}>{item.name}</div>
+            <div className={styles.linkBox}>
+              {item.relayproxy && <Link to={item.relayproxy}>
+                <button className={clsx( styles.button)}>
+                  <i className="fa-solid fa-server"></i> Configure the Relay Proxy
+                </button>
+              </Link>}
+              {item.gomodule && <Link to={item.gomodule}>
+                <button className={clsx( styles.button)}>
+                  <i className="devicon-go-original-wordmark"></i> Configure the GO Module
+                </button>
+              </Link>}
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
