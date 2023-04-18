4539b15937bb1acf49637af22f3c6c5b9e29c703
Dhanu Saputra <35093673+dhanusaputra@users.noreply.github.com>
2023-04-18 18:58:48 +0700

    feature support-parquet-for-data-collection (#667)

--- exporter/feature_event_test.go
+++ exporter/feature_event_test.go
@@ -45,3 +45,70 @@ func TestNewFeatureEvent(t *testing.T) {
 		})
 	}
 }
+
+func TestFeatureEvent_MarshalInterface(t *testing.T) {
+	tests := []struct {
+		name         string
+		featureEvent *exporter.FeatureEvent
+		want         *exporter.FeatureEvent
+		wantErr      bool
+	}{
+		{
+			name: "happy path",
+			featureEvent: &exporter.FeatureEvent{
+				Kind:         "feature",
+				ContextKind:  "anonymousUser",
+				UserKey:      "ABCD",
+				CreationDate: 1617970547,
+				Key:          "random-key",
+				Variation:    "Default",
+				Value: map[string]interface{}{
+					"string": "string",
+					"bool":   true,
+					"float":  1.23,
+					"int":    1,
+				},
+				Default: false,
+			},
+			want: &exporter.FeatureEvent{
+				Kind:         "feature",
+				ContextKind:  "anonymousUser",
+				UserKey:      "ABCD",
+				CreationDate: 1617970547,
+				Key:          "random-key",
+				Variation:    "Default",
+				Value:        `{"bool":true,"float":1.23,"int":1,"string":"string"}`,
+				Default:      false,
+			},
+		},
+		{
+			name: "marshal failed",
+			featureEvent: &exporter.FeatureEvent{
+				Kind:         "feature",
+				ContextKind:  "anonymousUser",
+				UserKey:      "ABCD",
+				CreationDate: 1617970547,
+				Key:          "random-key",
+				Variation:    "Default",
+				Value:        make(chan int),
+				Default:      false,
+			},
+			wantErr: true,
+		},
+		{
+			name:         "nil featureEvent",
+			featureEvent: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.featureEvent.MarshalInterface(); (err != nil) != tt.wantErr {
+				t.Errorf("FeatureEvent.MarshalInterface() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.want != nil {
+				assert.Equal(t, tt.want, tt.featureEvent)
+			}
+		})
+	}
+}
