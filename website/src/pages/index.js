886ccd1fb012b8fbfbe92e69b212238ab509cec5
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-04-21 20:49:09 +0200

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title="Seamless Feature Flagging for All"
+      title="Simple Feature Flagging for All"
       description={`${siteConfig.tagline}`}>
       <HomeHeader />
       <Whatis />
