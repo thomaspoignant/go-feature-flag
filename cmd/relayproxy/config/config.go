724acbef583a65de5234675c1705d188df6c2fef
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-05-06 18:35:25 +0200

    fix: CSV export format is broken (#769)
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- cmd/relayproxy/config/config.go
+++ cmd/relayproxy/config/config.go
@@ -44,8 +44,8 @@ var DefaultExporter = struct {
 	Format:    "JSON",
 	LogFormat: "[{{ .FormattedDate}}] user=\"{{ .UserKey}}\", flag=\"{{ .Key}}\", value=\"{{ .Value}}\"",
 	FileName:  "flag-variation-{{ .Hostname}}-{{ .Timestamp}}.{{ .Format}}",
-	CsvFormat: "{ .Kind}};{{ .ContextKind}};{{ .UserKey}};{{ .CreationDate}};{{ .Key}};{{ .Variation}};" +
-		"{{ .Value}};{{ .Default}}\\n",
+	CsvFormat: "{{ .Kind}};{{ .ContextKind}};{{ .UserKey}};{{ .CreationDate}};{{ .Key}};{{ .Variation}};" +
+		"{{ .Value}};{{ .Default}}\n",
 	FlushInterval:           60000 * time.Millisecond,
 	MaxEventInMemory:        100000,
 	ParquetCompressionCodec: parquet.CompressionCodec_SNAPPY.String(),
