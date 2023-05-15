ca7397d6c91826ee01e389d92dd638976cdce128
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-05-15 16:37:30 +0200

    Add tests
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- cmd/relayproxy/api/middleware/zap_test.go
+++ cmd/relayproxy/api/middleware/zap_test.go
@@ -13,7 +13,7 @@ import (
 	"go.uber.org/zap/zaptest/observer"
 )
 
-func TestZapLogger(t *testing.T) {
+func TestZapLogger200(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/something", nil)
 	rec := httptest.NewRecorder()
@@ -34,4 +34,106 @@ func TestZapLogger(t *testing.T) {
 	assert.Equal(t, "GET /something", logFields["request"])
 	assert.NotNil(t, logFields["host"])
 	assert.NotNil(t, logFields["size"])
+}
+
+func TestZapLogger300(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusFound, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+	logger := zap.New(obs)
+	err := middleware.ZapLogger(logger, &config.Config{})(h)(c)
+	assert.Nil(t, err)
+	logFields := logs.AllUntimed()[0].ContextMap()
+	assert.Equal(t, 1, logs.Len())
+	assert.Equal(t, int64(302), logFields["status"])
+	assert.NotNil(t, logFields["latency"])
+	assert.Equal(t, "GET /something", logFields["request"])
+	assert.NotNil(t, logFields["host"])
+	assert.NotNil(t, logFields["size"])
+}
+func TestZapLogger400(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusBadRequest, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+	logger := zap.New(obs)
+	err := middleware.ZapLogger(logger, &config.Config{})(h)(c)
+	assert.Nil(t, err)
+	logFields := logs.AllUntimed()[0].ContextMap()
+	assert.Equal(t, 1, logs.Len())
+	assert.Equal(t, int64(400), logFields["status"])
+	assert.NotNil(t, logFields["latency"])
+	assert.Equal(t, "GET /something", logFields["request"])
+	assert.NotNil(t, logFields["host"])
+	assert.NotNil(t, logFields["size"])
+}
+
+func TestZapLogger500(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+	logger := zap.New(obs)
+	err := middleware.ZapLogger(logger, &config.Config{})(h)(c)
+	assert.Nil(t, err)
+	logFields := logs.AllUntimed()[0].ContextMap()
+	assert.Equal(t, 1, logs.Len())
+	assert.Equal(t, int64(500), logFields["status"])
+	assert.NotNil(t, logFields["latency"])
+	assert.Equal(t, "GET /something", logFields["request"])
+	assert.NotNil(t, logFields["host"])
+	assert.NotNil(t, logFields["size"])
+}
+
+func TestZapLoggerHealth(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+	logger := zap.New(obs)
+	err := middleware.ZapLogger(logger, &config.Config{})(h)(c)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(logs.AllUntimed()))
+}
+
+func TestZapLoggerHealthDebug(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+	logger := zap.New(obs)
+	err := middleware.ZapLogger(logger, &config.Config{Debug: true})(h)(c)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(logs.AllUntimed()))
 }
