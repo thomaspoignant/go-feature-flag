79d0d4ce4f8b9477c799a59c83b82d4e65d5fa38
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-06-18 19:13:27 +0200

    Add tests for the convertion
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- cmd/relayproxy/controller/utils_test.go
+++ cmd/relayproxy/controller/utils_test.go
@@ -5,6 +5,8 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/thomaspoignant/go-feature-flag/cmd/relayproxy/model"
+	"github.com/thomaspoignant/go-feature-flag/ffcontext"
+	"github.com/thomaspoignant/go-feature-flag/ffuser"
 	"net/http"
 	"testing"
 )
@@ -24,5 +26,34 @@ func Test_assertRequest_request_without_user(t *testing.T) {
 func Test_userRequestToUser_nil_user(t *testing.T) {
 	_, got := userRequestToUser(nil)
 	want := fmt.Errorf("userRequestToUser: impossible to convert user, userRequest nil")
+	assert.Equal(t, want, got)
+}
+
+func Test_evaluationContextFromRequest_valid_use_case(t *testing.T) {
+	req := &model.AllFlagRequest{
+		User: nil,
+		EvaluationContext: &model.EvaluationContextRequest{
+			Key: "key-1",
+			Custom: map[string]interface{}{
+				"anonymous": false,
+			},
+		},
+	}
+	got, err := evaluationContextFromRequest(req)
+	want := ffcontext.NewEvaluationContextBuilder("key-1").AddCustom("anonymous", false).Build()
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
+
+func Test_evaluationContextFromRequest_user_entry(t *testing.T) {
+	req := &model.AllFlagRequest{
+		User: &model.UserRequest{
+			Key:       "key-1",
+			Anonymous: false,
+		},
+	}
+	got, err := evaluationContextFromRequest(req)
+	want := ffuser.NewUserBuilder("key-1").Anonymous(false).Build()
+	assert.NoError(t, err)
 	assert.Equal(t, want, got)
 }
