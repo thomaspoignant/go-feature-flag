e847a4fb98be7bfe38959c3dc470c08e77e50673
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-06-27 11:02:05 +0200

    Remove TODO + add swagger
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- cmd/relayproxy/controller/ws_flag_change.go
+++ cmd/relayproxy/controller/ws_flag_change.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/thomaspoignant/go-feature-flag/cmd/relayproxy/service"
@@ -31,7 +30,18 @@ type wsFlagChange struct {
 	logger           *zap.Logger
 }
 
-// TODO: add doc for swagger
+// Handler is the entry point for the websocket endpoint to get notified when a flag has been edited
+// @Summary      Websocket endpoint to be notified about flag changes
+// @Description  This endpoint is a websocket endpoint to be notified about flag changes, every change
+// @Description  will send a request to the client with a model.DiffCache format.
+// @Description
+// @Produce      json
+// @Accept		 json
+// @Param 	     apiKey query string false "apiKey use authorize the connection to the relay proxy"
+// @Success      200  {object} notifier.DiffCache "Success"
+// @Failure      400 {object} modeldocs.HTTPErrorDoc "Bad Request"
+// @Failure      500 {object} modeldocs.HTTPErrorDoc "Internal server error"
+// @Router       /ws/v1/flag/change [post]
 func (f *wsFlagChange) Handler(c echo.Context) error {
 	conn, err := f.upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
@@ -46,14 +56,12 @@ func (f *wsFlagChange) Handler(c echo.Context) error {
 	go f.pingPongLoop(conn)
 	isOpen := true
 	for isOpen {
-		_, msg, err := conn.ReadMessage()
+		_, _, err := conn.ReadMessage()
 		if err != nil {
 			f.websocketService.Deregister(conn)
 			f.logger.Debug("closing websocket connection", zap.Error(err), zap.Any("connection", conn))
 			isOpen = false
 		}
-		// TODO: remove line bellow
-		fmt.Println(string(msg))
 	}
 	return nil
 }
