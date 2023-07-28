696fa88c11d053b8ed8103070b7f04f3a87a613d
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-07-28 18:51:04 +0200

    Add jitter to accept randomization of the retrieval time (#943)
    * Add jitter to accept randomization of the retrieval time
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    * Add jitter to accept randomization of the retrieval time
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    * Remove example
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    * fix an issue if maxJitter is under 0
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    ---------
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- background_updater.go
+++ background_updater.go
@@ -1,6 +1,7 @@
 package ffclient
 
 import (
+	"math/rand"
 	"time"
 )
 
@@ -12,9 +13,24 @@ type backgroundUpdater struct {
 }
 
 // newBackgroundUpdater init default value for the ticker and the channel.
-func newBackgroundUpdater(pollingInterval time.Duration) backgroundUpdater {
+func newBackgroundUpdater(pollingInterval time.Duration, useJitter bool) backgroundUpdater {
+	tickerDuration := pollingInterval
+	if useJitter {
+		// we accept a deviation of maximum 10% of the polling interval
+		maxJitter := float64(pollingInterval) * 0.1
+		jitter := time.Duration(0)
+		if int64(maxJitter) > 0 {
+			jitter = time.Duration(rand.Int63n(int64(maxJitter))) // nolint: gosec
+		}
+		if jitter%2 == 0 {
+			tickerDuration = pollingInterval + jitter
+		} else {
+			tickerDuration = pollingInterval - jitter
+		}
+	}
+
 	return backgroundUpdater{
-		ticker:      time.NewTicker(pollingInterval),
+		ticker:      time.NewTicker(tickerDuration),
 		updaterChan: make(chan struct{}),
 	}
 }
