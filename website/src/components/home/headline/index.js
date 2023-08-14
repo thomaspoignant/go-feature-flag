0dcd65f624ca3580ae1414030ba6493f1ecd26f3
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-08-14 12:03:10 +0200

    Some UI improvement in the home page (#1009)
    * Some UI improvement in the home page
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    * Change order
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    ---------
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ website/src/components/home/headline/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export function Headline() {
+  return (
+    <div className={styles.headline}>
+      <div className={styles.title}>
+      <h1>Optimize Feature Launches with Smart Rollout Techniques.<br/>From <span className={styles.green}>Progressive Rollouts</span> to <span className={styles.purple}>Sequential Releases.</span></h1>
+      </div>
+    </div>
+  );
+}
