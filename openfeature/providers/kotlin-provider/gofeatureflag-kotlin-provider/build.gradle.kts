caa18b29d04fd75570c6625b5e83cb8608597153
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-11-04 21:38:36 +0100

    config graddle (#1214)

--- openfeature/providers/kotlin-provider/gofeatureflag-kotlin-provider/build.gradle.kts
+++ openfeature/providers/kotlin-provider/gofeatureflag-kotlin-provider/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("org.jlleitschuh.gradle.ktlint")
 }
 
 val releaseVersion = project.extra["version"].toString()
@@ -30,6 +32,55 @@ android {
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+}
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = project.extra["groupId"].toString()
+            artifactId = "gofeatureflag-kotlin-provider"
+            version = releaseVersion
+
+            pom {
+                name.set("GO Feature Flag OpenFeature Provider for Android")
+                description.set(
+                    "This is the Android provider implementation of OpenFeature for GO Feature Flag."
+                )
+                url.set("https://gofeatureflag.org")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("thomaspoignant")
+                        name.set("Thomas Poignant")
+                        email.set("thomas.poignant@gofeatureflag.org")
+                    }
+                }
+                scm {
+                    connection.set(
+                        "scm:git:https://github.com/thomaspoignant/go-feature-flag.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:ssh://git@github.com:thomaspoignant/go-feature-flag.git"
+                    )
+                    url.set("https://github.com/thomaspoignant/go-feature-flag/tree/main/openfeature/providers/kotlin-provider")
+                }
+            }
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
