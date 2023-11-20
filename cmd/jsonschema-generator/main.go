2af7beeffeb03590211c7bf6db518cfaf08ab6fc
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2023-11-20 17:47:31 +0100

    Fix schema generator (#1291)
    * Fix JSON schema generator
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    * Fix JSON schema generator
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    ---------
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- cmd/jsonschema-generator/main.go
+++ cmd/jsonschema-generator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/invopop/jsonschema"
 	"github.com/jessevdk/go-flags"
 	"github.com/thomaspoignant/go-feature-flag/internal/dto"
@@ -14,12 +15,14 @@ func main() {
 	var opts struct {
 		SchemaLocation string `long:"schema-location" description:"Location where the schema will be saved." required:"true"` //nolint: lll
 	}
+
 	_, err := flags.Parse(&opts)
-	if flags.WroteHelp(err) {
-		os.Exit(0)
-	}
 	if err != nil {
 		log.Fatal("impossible to parse command line parameters", err)
+	}
+
+	if opts.SchemaLocation == "" {
+		log.Fatal("schema-location is required")
 	}
 
 	a := map[string]dto.DTOv1{}
@@ -34,17 +37,18 @@ func main() {
 		log.Fatal("impossible to prettify jsonschema", err)
 	}
 
-	err = os.WriteFile(opts.SchemaLocation, []byte(prettyJSONSchema), 0600)
+	fmt.Println("Writing jsonschema to", opts.SchemaLocation)
+	err = os.WriteFile(opts.SchemaLocation, prettyJSONSchema, 0600)
 	if err != nil {
 		log.Fatalf("impossible to write jsonschema file to %s: %s", opts.SchemaLocation, err)
 	}
 }
 
 // PrettyString will prettify the JSON string
-func PrettyString(str string) (string, error) {
+func PrettyString(str string) ([]byte, error) {
 	var prettyJSON bytes.Buffer
 	if err := json.Indent(&prettyJSON, []byte(str), "", "    "); err != nil {
-		return "", err
+		return []byte(""), err
 	}
-	return prettyJSON.String(), nil
+	return prettyJSON.Bytes(), nil
 }
