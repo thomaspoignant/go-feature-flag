fd90988686cccb85f23cdedc39f7e2bc65c47380
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-01-05 09:19:20 +0100

    Support initializable retriever (#1427)
    * Support initializable retriever
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    
    * Update feature_flag_test.go
    
    Co-authored-by: Luiz Guilherme Ribeiro <ltrindaderibeiro@gmail.com>
    
    ---------
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    Co-authored-by: Luiz Guilherme Ribeiro <ltrindaderibeiro@gmail.com>

--- retriever/retriever.go
+++ retriever/retriever.go
@@ -9,3 +9,22 @@ type Retriever interface {
 	// Retrieve function is supposed to load the file and to return a []byte of your flag configuration file.
 	Retrieve(ctx context.Context) ([]byte, error)
 }
+
+// InitializableRetriever is an extended version of the retriever that can be initialized and shutdown.
+type InitializableRetriever interface {
+	Retrieve(ctx context.Context) ([]byte, error)
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+	Status() Status
+}
+
+// Status is the status of the retriever.
+// It can be used to check if the retriever is ready to be used.
+// If not ready, we wi will not use it.
+type Status = string
+
+const (
+	RetrieverReady    Status = "READY"
+	RetrieverNotReady Status = "NOT_READY"
+	RetrieverError    Status = "ERROR"
+)
