fc05b95bf9a3bb3ead1d2b64c9a01f4747cbd950
Carlos Silveira Junior <carlos@miro.com>
2024-01-17 10:57:14 -0300

    choro: add test for rule name on metadata

--- cmd/editor/main.go
+++ cmd/editor/main.go
@@ -59,21 +59,9 @@ func EvaluateHandler(c echo.Context) error {
 		ErrorCode:     resolutionDetails.ErrorCode,
 		Failed:        resolutionDetails.ErrorCode != "",
 		Cacheable:     resolutionDetails.Cacheable,
-		Metadata:      constructMetadata(f, resolutionDetails),
+		Metadata:      f.GetMetadata(),
 	}
 	return c.JSON(http.StatusOK, resp)
-}
-
-func constructMetadata(f flag.InternalFlag, resolutionDetails flag.ResolutionDetails) map[string]interface{} {
-	metadata := f.GetMetadata()
-	if resolutionDetails.RuleName == nil || *resolutionDetails.RuleName == "" {
-		return metadata
-	}
-	if metadata == nil {
-		metadata = make(map[string]interface{})
-	}
-	metadata["evaluatedRuleName"] = resolutionDetails.RuleName
-	return metadata
 }
 
 // HealthHandler endpoint to validate that the service is up and running.
