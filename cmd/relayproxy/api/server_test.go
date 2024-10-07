a03dd4aa596127f32ade2e5f1dd7bd299b6c6109
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-10-07 18:53:04 +0200

    Add test for OFREP routes

--- cmd/relayproxy/api/server_test.go
+++ cmd/relayproxy/api/server_test.go
@@ -2,6 +2,7 @@ package api_test
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -132,4 +133,87 @@ func Test_Starting_RelayProxy_with_monitoring_on_different_port(t *testing.T) {
 	responseI1, err := http.Get("http://localhost:11025/info")
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusOK, responseI1.StatusCode)
+}
+
+func Test_CheckOFREPAPIExists(t *testing.T) {
+	proxyConf := &config.Config{
+		Retriever: &config.RetrieverConf{
+			Kind: "file",
+			Path: "../../../testdata/flag-config.yaml",
+		},
+		ListenPort: 11024,
+		AuthorizedKeys: config.APIKeys{
+			Admin:      nil,
+			Evaluation: []string{"test"},
+		},
+	}
+	log := log.InitLogger()
+	defer func() { _ = log.ZapLogger.Sync() }()
+
+	metricsV2, err := metric.NewMetrics()
+	if err != nil {
+		log.ZapLogger.Error("impossible to initialize prometheus metrics", zap.Error(err))
+	}
+	wsService := service.NewWebsocketService()
+	defer wsService.Close() // close all the open connections
+	prometheusNotifier := metric.NewPrometheusNotifier(metricsV2)
+	proxyNotifier := service.NewNotifierWebsocket(wsService)
+	goff, err := service.NewGoFeatureFlagClient(proxyConf, log.ZapLogger, []notifier.Notifier{
+		prometheusNotifier,
+		proxyNotifier,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	services := service.Services{
+		MonitoringService:    service.NewMonitoring(goff),
+		WebsocketService:     wsService,
+		GOFeatureFlagService: goff,
+		Metrics:              metricsV2,
+	}
+
+	s := api.New(proxyConf, services, log.ZapLogger)
+	go func() { s.Start() }()
+	defer s.Stop()
+
+	time.Sleep(10 * time.Millisecond)
+
+	req, err := http.NewRequest("GET",
+		"http://localhost:11024/ofrep/v1/configuration", nil)
+	assert.NoError(t, err)
+	req.Header.Add("Authorization", "Bearer test")
+	response, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+
+	req, err = http.NewRequest("POST",
+		"http://localhost:11024/ofrep/v1/evaluate/flags",
+		strings.NewReader(`{ "context":{"targetingKey":"some-key"}}`))
+	assert.NoError(t, err)
+	req.Header.Add("Authorization", "Bearer test")
+	req.Header.Add("Content-Type", "application/json")
+	response, err = http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+
+	req, err = http.NewRequest("POST",
+		"http://localhost:11024/ofrep/v1/evaluate/flags/some-key",
+		strings.NewReader(`{ "context":{"targetingKey":"some-key"}}`))
+	assert.NoError(t, err)
+	req.Header.Add("Authorization", "Bearer test")
+	req.Header.Add("Content-Type", "application/json")
+	response, err = http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, response.StatusCode)
+
+	req, err = http.NewRequest("POST",
+		"http://localhost:11024/ofrep/v1/evaluate/flags/test-flag",
+		strings.NewReader(`{ "context":{"targetingKey":"some-key"}}`))
+	assert.NoError(t, err)
+	req.Header.Add("Authorization", "Bearer test")
+	req.Header.Add("Content-Type", "application/json")
+	response, err = http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
 }
