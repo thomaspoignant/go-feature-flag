a8d5ddf2abe2b4ddc769ebcf6962ad91397d7a9b
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-10-11 09:35:18 +0200

    feat: Support all type of AWS Lambda usage

--- cmd/relayproxy/api/lambda_handler.go
+++ cmd/relayproxy/api/lambda_handler.go
@@ -5,14 +5,13 @@ import (
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
 	"github.com/labstack/echo/v4"
 )
 
-// newAwsLambdaHandler is creating a new awsLambdaHandler struct with the echoadapter
+// newAwsLambdaHandlerManager is creating a new awsLambdaHandler struct with the echoadapter
 // to proxy all lambda event to echo.
-func newAwsLambdaHandler(echoInstance *echo.Echo) awsLambdaHandler {
+func newAwsLambdaHandlerManager(echoInstance *echo.Echo) awsLambdaHandler {
 	return awsLambdaHandler{
 		adapterApiGtwV2: echoadapter.NewV2(echoInstance),
 		adapterALB:      echoadapter.NewALB(echoInstance),
@@ -26,17 +25,14 @@ type awsLambdaHandler struct {
 	adapterALB      *echoadapter.EchoLambdaALB
 }
 
-func (h *awsLambdaHandler) Start(mode string) {
+func (h *awsLambdaHandler) GetAdapter(mode string) interface{} {
 	switch strings.ToUpper(mode) {
 	case "APIGATEWAYV1":
-		lambda.Start(h.HandlerApiGatewayV1)
-		return
+		return h.HandlerApiGatewayV1
 	case "ALB":
-		lambda.Start(h.HandlerALB)
-		return
+		return h.HandlerALB
 	default:
-		lambda.Start(h.HandlerApiGatewayV2)
-		return
+		return h.HandlerApiGatewayV2
 	}
 }
 
