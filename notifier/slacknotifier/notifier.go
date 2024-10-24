ab6f65a7093601676d86f2a373caa40ecbd62c8e
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-10-24 22:49:47 +0200

    chore: replace gdexlab/go-render by luci/go-render

--- notifier/slacknotifier/notifier.go
+++ notifier/slacknotifier/notifier.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gdexlab/go-render/render"
+	"github.com/luci/go-render/render"
 	"github.com/r3labs/diff/v3"
 	"github.com/thomaspoignant/go-feature-flag/internal"
 	"github.com/thomaspoignant/go-feature-flag/notifier"
