7a59dd19a7243ee4ffad5d080866ba0ac101645a
martin machiebe <martinmachiebe21@gmail.com>
2024-11-06 20:40:19 +0100

    updates

--- cmd/relayproxy/config/notifier.go
+++ cmd/relayproxy/config/notifier.go
@@ -5,12 +5,12 @@ import "fmt"
 type NotifierConf struct {
 	Kind NotifierKind `mapstructure:"kind" koanf:"kind"`
 	// Deprecated: Use WebhookURL instead
-	SlackWebhookURL          string              `mapstructure:"slackWebhookUrl" koanf:"slackWebhookUrl"`
-	EndpointURL              string              `mapstructure:"endpointUrl" koanf:"endpointUrl"`
-	Secret                   string              `mapstructure:"secret" koanf:"secret"`
-	Meta                     map[string]string   `mapstructure:"meta" koanf:"meta"`
-	Headers                  map[string][]string `mapstructure:"headers" koanf:"headers"`
-	WebhookURL               string              `mapstructure:"webhookUrl" koanf:"webhookurl"`
+	SlackWebhookURL string              `mapstructure:"slackWebhookUrl" koanf:"slackWebhookUrl"`
+	EndpointURL     string              `mapstructure:"endpointUrl" koanf:"endpointUrl"`
+	Secret          string              `mapstructure:"secret" koanf:"secret"`
+	Meta            map[string]string   `mapstructure:"meta" koanf:"meta"`
+	Headers         map[string][]string `mapstructure:"headers" koanf:"headers"`
+	WebhookURL      string              `mapstructure:"webhookUrl" koanf:"webhookurl"`
 }
 
 func (c *NotifierConf) IsValid() error {
