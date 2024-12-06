b790403354d768132481ff3790c972815f191feb
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-12-06 12:06:00 +0100

    docs(example): Fixing link in react example
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- examples/openfeature_react/react-app/src/App.tsx
+++ examples/openfeature_react/react-app/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { EvaluationContext, OpenFeature, OpenFeatureProvider, useFlag } from "@openfeature/react-sdk";
-import { GoFeatureFlagWebProvider } from "@openfeature/go-feature-flag-web-provider";
+import {EvaluationContext, OpenFeature, OpenFeatureProvider, useFlag} from "@openfeature/react-sdk";
+import {GoFeatureFlagWebProvider} from "@openfeature/go-feature-flag-web-provider";
 
 const goFeatureFlagWebProvider = new GoFeatureFlagWebProvider({
   endpoint: "http://localhost:1031"
@@ -15,35 +15,35 @@ OpenFeature.setProvider(goFeatureFlagWebProvider);
 function App() {
   return (
     <OpenFeatureProvider>
-      <Page />
+      <Page/>
     </OpenFeatureProvider>
   );
 }
 
 function Page() {
-  const { value: hideLogo } = useFlag("hide-logo", false);
-  const { value: titleFlag } = useFlag("title-flag", "GO Feature Flag");
-  const { value: badgeClass } = useFlag("badge-class", "");
+  const {value: hideLogo} = useFlag("hide-logo", false);
+  const {value: titleFlag} = useFlag("title-flag", "GO Feature Flag");
+  const {value: badgeClass} = useFlag("badge-class", "");
 
   const setRandomEvaluationContext = () => {
     const availableContexts: Array<EvaluationContext> = [
-      { targetingKey: "user-2", userType: "dev", email: "contact@gofeatureflag.org" },
-      { targetingKey: "user-3", userType: "admin", company: "GO Feature Flag" },
-      { targetingKey: "user-4", userType: "customer", location: "Paris" },
-      { targetingKey: "user-5" }
+      {targetingKey: "user-2", userType: "dev", email: "contact@gofeatureflag.org"},
+      {targetingKey: "user-3", userType: "admin", company: "GO Feature Flag"},
+      {targetingKey: "user-4", userType: "customer", location: "Paris"},
+      {targetingKey: "user-5"}
 
     ];
     const randomIndex = Math.floor(Math.random() * availableContexts.length);
     OpenFeature.setContext(availableContexts[randomIndex]);
   };
 
-  const userType =OpenFeature.getContext().userType ?? "anonymous" as string;
+  const userType = OpenFeature.getContext().userType ?? "anonymous" as string;
 
   return (
     <>
       <div>
         <a href="https://gofeatureflag.org" target="_blank">
-          {!hideLogo && <img src="/public/logo.png" className="logo" alt="GO Feature Flag logo" />}
+          {!hideLogo && <img src="/public/logo.png" className="logo" alt="GO Feature Flag logo"/>}
         </a>
       </div>
       <h2>React example app</h2>
