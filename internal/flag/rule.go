dcb69ed3b5950e7c57d5e72de68a781a0eca763f
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-12-13 19:34:39 +0100

    chore: simplify query
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- internal/flag/rule.go
+++ internal/flag/rule.go
@@ -233,20 +233,8 @@ func (r *Rule) IsValid(defaultRule bool, variations map[string]*interface{}) err
 	}
 
 	// targeting without query
-	if !defaultRule {
-		if r.Query == nil {
-			return fmt.Errorf("each targeting should have a query")
-		}
-
-		// Validate the query with the parser
-		ev, err := parser.NewEvaluator(*r.Query)
-		if err != nil {
-			return err
-		}
-		_, err = ev.Process(map[string]interface{}{})
-		if err != nil {
-			return fmt.Errorf("invalid query: %w", err)
-		}
+	if err := r.isQueryValid(defaultRule); err != nil {
+		return err
 	}
 
 	// Validate the percentage of the rule
@@ -292,6 +280,27 @@ func (r *Rule) IsValid(defaultRule bool, variations map[string]*interface{}) err
 		if _, ok := variations[r.GetVariationResult()]; !ok {
 			return fmt.Errorf("invalid variation: %s does not exist", r.GetVariationResult())
 		}
+	}
+	return nil
+}
+
+func (r *Rule) isQueryValid(defaultRule bool) error {
+	if defaultRule {
+		return nil
+	}
+
+	if r.Query == nil {
+		return fmt.Errorf("each targeting should have a query")
+	}
+
+	// Validate the query with the parser
+	ev, err := parser.NewEvaluator(*r.Query)
+	if err != nil {
+		return err
+	}
+	_, err = ev.Process(map[string]interface{}{})
+	if err != nil {
+		return fmt.Errorf("invalid query: %w", err)
 	}
 	return nil
 }
