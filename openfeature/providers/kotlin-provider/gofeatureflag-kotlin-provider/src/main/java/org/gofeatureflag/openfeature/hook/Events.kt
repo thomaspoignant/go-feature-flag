cd78ef46ca463df1c717bd2b5b2c503ad08f1122
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-01-15 09:43:14 +0100

    feat: Support exporter metadata in Android provider
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- openfeature/providers/kotlin-provider/gofeatureflag-kotlin-provider/src/main/java/org/gofeatureflag/openfeature/hook/Events.kt
+++ openfeature/providers/kotlin-provider/gofeatureflag-kotlin-provider/src/main/java/org/gofeatureflag/openfeature/hook/Events.kt
@@ -3,5 +3,5 @@ package org.gofeatureflag.openfeature.hook
 
 data class Events(
     val events: List<Event>?,
-    val meta: Map<String, String> = mapOf("provider" to "android", "openfeature" to "true")
+    val meta: Map<String, Any> = emptyMap()
 )
