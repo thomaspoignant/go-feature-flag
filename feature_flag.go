1c25ddeeb05ff262d4c7aeb734652425f78593a5
Augustinas <augustinas.sueris@vinted.com>
2025-04-01 15:28:06 +0300

    perf: Separate goroutine for exporter flow (#3272)
    * Separate goroutine for exporter.                                                                                                                      ✔  at 11:20:08 
    Service variant responses is not affected by slow exporter responses anymore.
    
    * update flow
    
    * Moved wg to FF struct
    
    * fixed flaky test

--- feature_flag.go
+++ feature_flag.go
@@ -48,6 +48,7 @@ type GoFeatureFlag struct {
 	bgUpdater        backgroundUpdater
 	dataExporter     exporter.Manager[exporter.FeatureEvent]
 	retrieverManager *retriever.Manager
+	exporterWg       sync.WaitGroup
 }
 
 // ff is the default object for go-feature-flag
@@ -354,5 +355,6 @@ func ForceRefresh() bool {
 // Close the component by stopping the background refresh and clean the cache.
 func Close() {
 	onceFF = sync.Once{}
+	ff.exporterWg.Wait()
 	ff.Close()
 }
