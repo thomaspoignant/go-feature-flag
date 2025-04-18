ca71e5af2c01108d70310b687b1468c41b1d24b1
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-04-18 09:35:29 +0200

    feat(relay-proxy): allow to set a prefix for env variables (#3337)

--- cmd/relayproxy/config/config.go
+++ cmd/relayproxy/config/config.go
@@ -100,7 +100,7 @@ func New(flagSet *pflag.FlagSet, log *zap.Logger, version string) (*Config, erro
 	}
 
 	// Map environment variables
-	_ = k.Load(mapEnvVariablesProvider(log), nil)
+	_ = k.Load(mapEnvVariablesProvider(k.String("envVariablePrefix"), log), nil)
 	_ = k.Set("version", version)
 
 	proxyConf := &Config{}
@@ -117,52 +117,46 @@ func New(flagSet *pflag.FlagSet, log *zap.Logger, version string) (*Config, erro
 		}
 	}
 
-	if proxyConf.Debug {
-		log.Warn(
-			"Option Debug that you are using in your configuration file is deprecated" +
-				"and will be removed in future versions." +
-				"Please use logLevel: debug to continue to run the relay-proxy with debug logs.")
-	}
-
 	return proxyConf, nil
 }
 
-func mapEnvVariablesProvider(log *zap.Logger) koanf.Provider {
-	return env.ProviderWithValue("", ".", func(s string, v string) (string, interface{}) {
-		if strings.HasPrefix(s, "RETRIEVERS") ||
-			strings.HasPrefix(s, "NOTIFIERS") ||
-			strings.HasPrefix(s, "EXPORTERS") {
+func mapEnvVariablesProvider(prefix string, log *zap.Logger) koanf.Provider {
+	return env.ProviderWithValue(prefix, ".", func(key string, v string) (string, interface{}) {
+		key = strings.TrimPrefix(key, prefix)
+		if strings.HasPrefix(key, "RETRIEVERS") ||
+			strings.HasPrefix(key, "NOTIFIERS") ||
+			strings.HasPrefix(key, "EXPORTERS") {
 			configMap := k.Raw()
-			err := loadArrayEnv(s, v, configMap)
+			err := loadArrayEnv(key, v, configMap)
 			if err != nil {
 				log.Error(
 					"config: error loading array env",
-					zap.String("key", s),
+					zap.String("key", key),
 					zap.String("value", v),
 					zap.Error(err),
 				)
-				return s, v
+				return key, v
 			}
-			return s, v
+			return key, v
 		}
 
-		if strings.HasPrefix(s, "EXPORTER_KAFKA_ADDRESSES") {
+		if strings.HasPrefix(key, "EXPORTER_KAFKA_ADDRESSES") {
 			return "exporter.kafka.addresses", strings.Split(v, ",")
 		}
 
-		if strings.HasPrefix(s, "AUTHORIZEDKEYS_EVALUATION") {
+		if strings.HasPrefix(key, "AUTHORIZEDKEYS_EVALUATION") {
 			return "authorizedKeys.evaluation", strings.Split(v, ",")
 		}
-		if strings.HasPrefix(s, "AUTHORIZEDKEYS_ADMIN") {
+		if strings.HasPrefix(key, "AUTHORIZEDKEYS_ADMIN") {
 			return "authorizedKeys.admin", strings.Split(v, ",")
 		}
 
-		if s == "OTEL_RESOURCE_ATTRIBUTES" {
+		if key == "OTEL_RESOURCE_ATTRIBUTES" {
 			parseOtelResourceAttributes(v, log)
-			return s, v
+			return key, v
 		}
 
-		return strings.ReplaceAll(strings.ToLower(s), "_", "."), v
+		return strings.ReplaceAll(strings.ToLower(key), "_", "."), v
 	})
 }
 
@@ -213,11 +207,6 @@ type Config struct {
 
 	// HideBanner (optional) if true, we don't display the go-feature-flag relay proxy banner
 	HideBanner bool `mapstructure:"hideBanner" koanf:"hidebanner"`
-
-	// Debug (optional) if true, go-feature-flag relay proxy will run on debug mode, with more logs and custom responses.
-	// It will also start the pprof endpoints on the same port as the monitoring.
-	// Default: false
-	Debug bool `mapstructure:"debug" koanf:"debug"`
 
 	// EnablePprof (optional) if true, go-feature-flag relay proxy will start
 	// the pprof endpoints on the same port as the monitoring.
@@ -347,6 +336,10 @@ type Config struct {
 
 	// JaegerConfig is the configuration for the Jaeger sampling of the relay proxy
 	JaegerConfig JaegerSamplerConfiguration `mapstructure:"jaeger" koanf:"jaeger"`
+
+	// EnvVariablePrefix (optional) is the prefix we are using to load the environment variables
+	// By default we have no prefix
+	EnvVariablePrefix string `mapstructure:"envVariablePrefix" koanf:"envvariableprefix"`
 
 	// ---- private fields
 
@@ -623,18 +616,13 @@ func (c *Config) IsDebugEnabled() bool {
 	if c == nil {
 		return false
 	}
-	return strings.ToLower(c.LogLevel) == "debug" || c.Debug
+	return strings.ToLower(c.LogLevel) == "debug"
 }
 
 func (c *Config) ZapLogLevel() zapcore.Level {
 	if c == nil {
 		return zapcore.InvalidLevel
 	}
-	// Use debug flag for backward compatibility
-	if c.Debug {
-		return zapcore.DebugLevel
-	}
-
 	level, err := zapcore.ParseLevel(c.LogLevel)
 	if err != nil {
 		return zapcore.InvalidLevel
