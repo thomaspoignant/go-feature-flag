79b6bd492a03f225a083b27e9ccb5cf89530ae7e
deamondev <piotr.rudnicki94@protonmail.com>
2025-09-17 17:13:59 +0200

    use retriever factory
    Signed-off-by: deamondev <piotr.rudnicki94@protonmail.com>

--- cmdhelpers/retrieverconf/init/retriever_init.go
+++ cmdhelpers/retrieverconf/init/retriever_init.go
@@ -20,33 +20,9 @@ func InitRetriever(
 	if c.Timeout != 0 {
 		defaultRetrieverConfig.Timeout = time.Duration(c.Timeout) * time.Millisecond
 	}
-	switch c.Kind {
-	case retrieverconf.GitHubRetriever:
-		return createGitHubRetriever(c, &defaultRetrieverConfig)
-	case retrieverconf.GitlabRetriever:
-		return createGitlabRetriever(c, &defaultRetrieverConfig)
-	case retrieverconf.BitbucketRetriever:
-		return createBitbucketRetriever(c, &defaultRetrieverConfig)
-	case retrieverconf.FileRetriever:
-		return createFileRetriever(c, nil)
-	case retrieverconf.S3Retriever:
-		return createS3Retriever(c, nil)
-	case retrieverconf.HTTPRetriever:
-		return createHTTPRetriever(c, &defaultRetrieverConfig)
-	case retrieverconf.GoogleStorageRetriever:
-		return createGoogleStorageRetriever(c, nil)
-	case retrieverconf.KubernetesRetriever:
-		return createKubernetesRetriever(c, nil)
-	case retrieverconf.MongoDBRetriever:
-		return createMongoDBRetriever(c, nil)
-	case retrieverconf.RedisRetriever:
-		return createRedisRetriever(c, nil)
-	case retrieverconf.AzBlobStorageRetriever:
-		return createAzBlobStorageRetriever(c, nil)
-	case retrieverconf.PostgreSQLRetriever:
-		return createPostgreSQLRetriever(c, nil)
-	default:
-		return nil, fmt.Errorf("invalid retriever: kind \"%s\" "+
-			"is not supported", c.Kind)
+	retrieverFactory, exists := retrieverFactories[c.Kind]
+	if !exists {
+		return nil, fmt.Errorf("invalid retriever: kind \"%s\" is not supported", c.Kind)
 	}
+	return retrieverFactory(c, &defaultRetrieverConfig)
 }
