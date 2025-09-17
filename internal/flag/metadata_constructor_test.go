ea90ba0f4f26c39edf35cf7649f3c4d87b635531
Carlos Silveira Junior <carlos@miro.com>
2025-09-17 10:55:07 +0200

    test: testing metadata constructor

--- internal/flag/metadata_constructor_test.go
+++ internal/flag/metadata_constructor_test.go
@@ -5,6 +5,7 @@ package flag
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"runtime"
 	"testing"
 )
@@ -42,4 +43,65 @@ func Test_constructMetadataParallel(t *testing.T) {
 			assert.Equal(t, tt.wantEvaluatedRuleName, got["evaluatedRuleName"])
 		})
 	}
+}
+
+func Test_ConstructMetadata(t *testing.T) {
+	rule := "test-rule"
+
+	tests := []struct {
+		name         string
+		flagMetadata map[string]any
+		ruleName     *string
+		want         map[string]any
+	}{
+		{
+			name:         "nil metadata, nil rule",
+			flagMetadata: nil,
+			ruleName:     nil,
+			want:         nil,
+		},
+		{
+			name:         "nil metadata, empty rule",
+			flagMetadata: nil,
+			ruleName:     stringToPointer(""),
+			want:         nil,
+		},
+		{
+			name:         "nil metadata, non-empty rule",
+			flagMetadata: nil,
+			ruleName:     &rule,
+			want:         map[string]any{"evaluatedRuleName": "test-rule"},
+		},
+		{
+			name:         "non-nil metadata, nil rule",
+			flagMetadata: map[string]any{"foo": "bar"},
+			ruleName:     nil,
+			want:         map[string]any{"foo": "bar"},
+		},
+		{
+			name:         "non-nil metadata, empty rule",
+			flagMetadata: map[string]any{"foo": "bar"},
+			ruleName:     stringToPointer(""),
+			want:         map[string]any{"foo": "bar"},
+		},
+		{
+			name:         "non-nil metadata, non-empty rule",
+			flagMetadata: map[string]any{"foo": "bar"},
+			ruleName:     &rule,
+			want:         map[string]any{"foo": "bar", "evaluatedRuleName": "test-rule"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := constructMetadata(tt.flagMetadata, tt.ruleName)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("constructMetadata() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func stringToPointer(s string) *string {
+	return &s
 }
