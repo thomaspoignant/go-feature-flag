53cfac012e575321816c25bc4e48d915f2679d9e
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-10-10 22:42:33 +0200

    fix(test): validate that test panics (#4037)
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- to_delete_test.go
+++ to_delete_test.go
@@ -3,11 +3,12 @@ package ffclient_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/thomaspoignant/go-feature-flag/modules/core"
 	"github.com/thomaspoignant/go-feature-flag/modules/evaluation"
 )
 
 func TestToDelete(t *testing.T) {
-	core.Core()
-	evaluation.Evaluation()
+	assert.Panics(t, func() { core.Core() }, "core.Core() should panic")
+	assert.Panics(t, func() { evaluation.Evaluation() }, "evaluation.Evaluation() should panic")
 }
