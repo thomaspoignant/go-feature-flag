3b6356558f1016c881800edaa9b46ddede6f175e
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-10-30 17:50:22 +0000

    create custom error when nested key not found

--- modules/core/utils/nested_field.go
+++ modules/core/utils/nested_field.go
@@ -1,15 +1,16 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
+
+	"github.com/thomaspoignant/go-feature-flag/modules/core/internalerror"
 )
 
 // GetNestedFieldValue returns the value from a nested path in the given map.
 // If the path does not exist or an error occurs, it returns an error.
 func GetNestedFieldValue(ctx map[string]interface{}, bucketingKey string) (interface{}, error) {
 	if ctx == nil || bucketingKey == "" {
-		return nil, fmt.Errorf("nested key not found: %s", bucketingKey)
+		return nil, &internalerror.NestedKeyNotFoundError{Key: bucketingKey}
 	}
 
 	parts := strings.Split(bucketingKey, ".")
@@ -18,12 +19,12 @@ func GetNestedFieldValue(ctx map[string]interface{}, bucketingKey string) (inter
 	for _, part := range parts {
 		m, ok := current.(map[string]interface{})
 		if !ok {
-			return nil, fmt.Errorf("nested key not found: %s", bucketingKey)
+			return nil, &internalerror.NestedKeyNotFoundError{Key: bucketingKey}
 		}
 
 		current, ok = m[part]
 		if !ok {
-			return nil, fmt.Errorf("nested key not found: %s", bucketingKey)
+			return nil, &internalerror.NestedKeyNotFoundError{Key: bucketingKey}
 		}
 	}
 	return current, nil
