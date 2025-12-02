4617a05e1336b72d5961e194d4cc1173d1beb246
Georgi Dimitrov <georgi.dimitrov@swisscom.com>
2025-12-02 23:05:55 +0200

    format

--- retriever/postgresqlretriever/database.go
+++ retriever/postgresqlretriever/database.go
@@ -8,43 +8,42 @@ import (
 )
 
 var (
-    pool     *pgxpool.Pool
-    mu       sync.Mutex
-    refCount int
+	pool     *pgxpool.Pool
+	mu       sync.Mutex
+	refCount int
 )
 
 func GetPool(ctx context.Context, uri string) (*pgxpool.Pool, error) {
-    mu.Lock()
-    defer mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 
-    if pool == nil {
-        p, err := pgxpool.New(ctx, uri)
-        if err != nil {
-            return nil, err
-        }
-        if err := p.Ping(ctx); err != nil {
-            p.Close()
-            return nil, err
-        }
+	if pool == nil {
+		p, err := pgxpool.New(ctx, uri)
+		if err != nil {
+			return nil, err
+		}
+		if err := p.Ping(ctx); err != nil {
+			p.Close()
+			return nil, err
+		}
 
-        pool = p
-    }
+		pool = p
+	}
 
-    refCount++
-    return pool, nil
+	refCount++
+	return pool, nil
 }
 
 func ReleasePool() {
-    mu.Lock()
-    defer mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 
-    refCount--
-    if refCount <= 0 {
-        if pool != nil {
-            pool.Close()
-            pool = nil
-        }
-        refCount = 0
-    }
+	refCount--
+	if refCount <= 0 {
+		if pool != nil {
+			pool.Close()
+			pool = nil
+		}
+		refCount = 0
+	}
 }
-
