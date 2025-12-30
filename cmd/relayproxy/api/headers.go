ceaff67192ce38695169f938fbd2888b75daac2d
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-12-30 19:00:14 +0100

    refactor: consolidate header constants in helper package
    Removed duplicate headers.go file from api package and kept constants only in helper/const.go.
    
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- cmd/relayproxy/api/headers.go
+++
@@ -1,6 +0,0 @@
-package api
-
-const XAPIKeyHeader = "X-API-Key"
-const ContentTypeHeader = "Content-Type"
-const AuthorizationHeader = "Authorization"
-const BearerPrefix = "Bearer "
--- cmd/relayproxy/helper/const.go
+++ cmd/relayproxy/helper/const.go
@@ -1,6 +1,8 @@
 package helper
 
-const XAPIKeyHeader = "X-API-Key"
+// XAPIKeyHeader defines the header name for passing API keys.
+// This is not a credential itself, but a standard HTTP header key.
+const XAPIKeyHeader = "X-API-Key" // nolint: gosec
 const ContentTypeHeader = "Content-Type"
 const AuthorizationHeader = "Authorization"
 const BearerPrefix = "Bearer "
