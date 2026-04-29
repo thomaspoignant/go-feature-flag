6449b22a795afed823a4d93c4898114d5489fdf1
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2026-04-29 13:58:23 +0200

    adding tests
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- modules/core/dto/converter_test.go
+++ modules/core/dto/converter_test.go
@@ -148,3 +148,151 @@ func TestConvertV1DtoToInternalFlag(t *testing.T) {
 		})
 	}
 }
+
+func TestConvertInternalFlagToDto(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    flag.InternalFlag
+		expected dto.DTO
+	}{
+		{
+			name: "all fields set",
+			input: flag.InternalFlag{
+				BucketingKey: testconvert.String("bucketKey"),
+				Variations: &map[string]*any{
+					"var1": testconvert.Interface("var1"),
+					"var2": testconvert.Interface("var2"),
+				},
+				Rules: &[]flag.Rule{
+					{
+						Name:        testconvert.String("rule1"),
+						Query:       testconvert.String("key eq \"key\""),
+						Percentages: &map[string]float64{"var_true": 100, "var_false": 0},
+					},
+					{
+						Name:  testconvert.String("rule2"),
+						Query: testconvert.String("key eq \"key2\""),
+						ProgressiveRollout: &flag.ProgressiveRollout{
+							Initial: &flag.ProgressiveRolloutStep{
+								Variation:  testconvert.String("var1"),
+								Percentage: testconvert.Float64(30),
+								Date:       testconvert.Time(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+							},
+							End: &flag.ProgressiveRolloutStep{
+								Variation:  testconvert.String("var2"),
+								Percentage: testconvert.Float64(70),
+								Date:       testconvert.Time(time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)),
+							},
+						},
+					},
+					{
+						Name:            testconvert.String("rule3"),
+						Query:           testconvert.String("key eq \"key3\""),
+						VariationResult: testconvert.String("var2"),
+					},
+				},
+				DefaultRule: &flag.Rule{
+					VariationResult: testconvert.String("var1"),
+				},
+				Scheduled: &[]flag.ScheduledStep{
+					{
+						Date: testconvert.Time(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+						InternalFlag: flag.InternalFlag{
+							DefaultRule: &flag.Rule{
+								VariationResult: testconvert.String("var2"),
+							},
+						},
+					},
+				},
+				Experimentation: &flag.ExperimentationRollout{
+					Start: testconvert.Time(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+					End:   testconvert.Time(time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC)),
+				},
+				Metadata:    &map[string]any{"key": "value"},
+				TrackEvents: testconvert.Bool(true),
+				Disable:     testconvert.Bool(false),
+				Version:     testconvert.String("v1"),
+			},
+			expected: dto.DTO{
+				BucketingKey: testconvert.String("bucketKey"),
+				Variations: &map[string]*any{
+					"var1": testconvert.Interface("var1"),
+					"var2": testconvert.Interface("var2"),
+				},
+				Rules: &[]flag.Rule{
+					{
+						Name:        testconvert.String("rule1"),
+						Query:       testconvert.String("key eq \"key\""),
+						Percentages: &map[string]float64{"var_true": 100, "var_false": 0},
+					},
+					{
+						Name:  testconvert.String("rule2"),
+						Query: testconvert.String("key eq \"key2\""),
+						ProgressiveRollout: &flag.ProgressiveRollout{
+							Initial: &flag.ProgressiveRolloutStep{
+								Variation:  testconvert.String("var1"),
+								Percentage: testconvert.Float64(30),
+								Date:       testconvert.Time(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+							},
+							End: &flag.ProgressiveRolloutStep{
+								Variation:  testconvert.String("var2"),
+								Percentage: testconvert.Float64(70),
+								Date:       testconvert.Time(time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)),
+							},
+						},
+					},
+					{
+						Name:            testconvert.String("rule3"),
+						Query:           testconvert.String("key eq \"key3\""),
+						VariationResult: testconvert.String("var2"),
+					},
+				},
+				DefaultRule: &flag.Rule{
+					VariationResult: testconvert.String("var1"),
+				},
+				Scheduled: &[]flag.ScheduledStep{
+					{
+						Date: testconvert.Time(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+						InternalFlag: flag.InternalFlag{
+							DefaultRule: &flag.Rule{
+								VariationResult: testconvert.String("var2"),
+							},
+						},
+					},
+				},
+				Experimentation: &dto.ExperimentationDto{
+					Start: testconvert.Time(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+					End:   testconvert.Time(time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC)),
+				},
+				Metadata:    &map[string]any{"key": "value"},
+				TrackEvents: testconvert.Bool(true),
+				Disable:     testconvert.Bool(false),
+				Version:     testconvert.String("v1"),
+			},
+		},
+		{
+			name: "nil experimentation",
+			input: flag.InternalFlag{
+				Experimentation: nil,
+				Version:         testconvert.String("v1"),
+			},
+			expected: dto.DTO{
+				Experimentation: &dto.ExperimentationDto{},
+				Version:         testconvert.String("v1"),
+			},
+		},
+		{
+			name:  "empty input",
+			input: flag.InternalFlag{},
+			expected: dto.DTO{
+				Experimentation: &dto.ExperimentationDto{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := dto.ConvertInternalFlagToDto(tt.input)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
