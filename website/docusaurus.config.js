9555d11fbcadf166a33ddf1a526f6a0c77d99e36
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2026-08-13 21:53:20 +0200

    Add blog sidebar title and count configuration (#5832)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -298,6 +298,8 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
